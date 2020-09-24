a43ddd4244c2e34fcb2ebea1a91bc989ea46befc
Juan Cardenas <cjuanronal@gmail.com>
2020-09-24 00:38:22 -0500

    Mepeo de tablas en clases

--- src/main/java/com/platzi/market/web/controller/HolaMundoController.java
+++ src/main/java/com/platzi/market/web/controller/HolaMundoController.java
@@ -1,11 +1,14 @@
 package com.platzi.market.web.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/saludar")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class HolaMundoController {
 
     @GetMapping("/hola")
